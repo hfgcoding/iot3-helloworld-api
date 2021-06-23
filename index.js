b60940e333cfff25bbeaf76baec2351bbcf9ae92
Florian Geiselhart <19553942+fg-uulm@users.noreply.github.com>
2021-06-23 10:26:00 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
 const port = 3000
+
+app.use(cors())
 
 app.get('/api/v1/hello', (req, res) => {
   res.status(200).send({ msg: 'Hello World!', secret: process.env.SECRET }).end()
