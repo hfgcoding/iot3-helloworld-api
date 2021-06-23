0b9709dd66417e332ab1ff0c734a27dd671a8eac
Florian Geiselhart <19553942+fg-uulm@users.noreply.github.com>
2021-06-23 10:09:15 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 3000
 
 app.get('/api/v1/hello', (req, res) => {
-  res.status(200).send({ msg: 'Hello World!'}).end()
+  res.status(200).send({ msg: 'Hello World!', secret: process.env.SECRET }).end()
 })
 
 app.listen(port, () => {
